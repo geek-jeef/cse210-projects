6378f4d581b5902fb512b41d2050accef0f9b28c
Guy-Jeef TUMBA <guyjeef16@gmail.com>
2023-02-28 19:45:13 +0000

    Learning Activity C# Prep 3 with Stretch Challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+internal class NewBaseType
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        string playGame = "yes";
+
+        do
+        {
+            Random randomNumberGenerator = new Random();
+            int magicNumber = randomNumberGenerator.Next(1, 101);
+
+            int guess = -1;
+            int attempts = 1;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                    attempts++;
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                    attempts++;
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! after {attempts} attempts");
+                    Console.Write("Do you want to continue (yes/no) ? ");
+                    playGame = Console.ReadLine();
+                }
+            }
+
+        }while (playGame == "yes");
+
+        Console.WriteLine("Good Bye");
+
     }
 }
