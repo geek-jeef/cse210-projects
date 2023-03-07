8e10382b5a23a515dabb6932ff3fda5e720a1b14
Guy-Jeef TUMBA <guyjeef16@gmail.com>
2023-03-07 22:57:35 +0000

    W03 Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+        first.SetTop(22);
+        first.SetBottom(7);
+        Console.WriteLine(first.GetTop());
+        Console.WriteLine(first.GetBottom());
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+
+        Fraction second = new Fraction(3);
+        Console.WriteLine(second.GetTop());
+        Console.WriteLine(second.GetBottom());
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+
+        Fraction third = new Fraction(1, 4);
+        Console.WriteLine(third.GetTop());
+        Console.WriteLine(third.GetBottom());
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
     }
 }
